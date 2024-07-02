200b695652957cd7246accdf678aa6cde73a40f3
lllzzzxxx <487793904@qq.com>
2024-07-02 22:58:02 +0800

    redis实现全局唯一id

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,30 @@
+package com.hmdp.utils;
+
+import io.lettuce.core.TimeoutOptions;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+    private StringRedisTemplate stringRedisTemplate;
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+    public long nextId(String keyPrefix) {
+//        生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+//        生成序列号
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        Long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+
+        return timestamp << 32 | count;
+    }
+}
