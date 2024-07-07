29dacfb898db503fa84ead5714d827f8d1038293
lllzzzxxx <487793904@qq.com>
2024-07-07 22:43:05 +0800

    附近商铺

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -4,14 +4,20 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
 
 @MapperScan("com.hmdp.mapper")
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
 public class HmDianPingApplication {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
     public static void main(String[] args) {
         SpringApplication.run(HmDianPingApplication.class, args);
     }
+
 
 }
